219f33991c82d8c847da8a2ff369308e0672fd5b
munir sebikaaka <munirsebikaaka@gmail.com>
2024-06-05 09:57:36 +0300

    added classes sytax in the code

--- script.js
+++ script.js
@@ -10,45 +10,105 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (postion) {
-      const latitude = postion.coords.latitude;
-      const longitude = postion.coords.longitude;
-      console.log(latitude);
-      console.log(
-        longitude,
-        `https://www.google.com/maps/@${latitude},${longitude}`
+
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  constructor(coord, distance, duration) {
+    this.coord = coord;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+class Running extends Workout {
+  constructor(coord, distance, duration, cadence) {
+    super(coord, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coord, distance, duration, elevetionGain) {
+    super(coord, distance, duration);
+    this.elevetionGain = elevetionGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this.getPosition();
+    form.addEventListener('submit', this.newWorkout.bind(this));
+    inputType.addEventListener('change', this.toogleElevetionFields);
+  }
+  getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this.loadMap.bind(this),
+        function () {
+          alert('coulnt find ur location');
+        }
       );
-      const coords = [latitude, longitude];
-      const map = L.map('map').setView(coords, 13);
+  }
+  loadMap(postion) {
+    const latitude = postion.coords.latitude;
+    const longitude = postion.coords.longitude;
+    console.log(latitude);
+    console.log(
+      longitude,
+      `https://www.google.com/maps/@${latitude},${longitude}`
+    );
+    const coords = [latitude, longitude];
+    this.#map = L.map('map').setView(coords, 13);
 
-      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
 
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        const latlng = mapEvent.latlng;
-        const lat = latlng.lat;
-        const lng = latlng.lng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('codesmann workout')
-          .openPopup();
-      });
-    },
-    function () {
-      alert('coulnt find ur location');
-    }
-  );
+    this.#map.on('click', this.showForm.bind(this));
+  }
+  showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+  toogleElevetionFields() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+  newWorkout(e) {
+    e.preventDefault();
+    inputCadence.value =
+      inputDuration.value =
+      inputDistance.value =
+      inputElevation.value =
+        '';
+    const latlng = this.#mapEvent.latlng;
+    const lat = latlng.lat;
+    const lng = latlng.lng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('codesmann workout')
+      .openPopup();
+  }
+}
+const app = new App();
